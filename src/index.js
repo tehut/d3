c94faacc85a3198fe3f1e7c3ac003b0f84990722
Tehut <tehut.m.g@gmail.com>
2017-08-21 17:08:30 -0700

    images and node tree with image for root

--- src/index.js
+++ src/index.js
@@ -1,4 +1,108 @@
 var  d3 = require ('d3')
-
+var $ = require('jquery')
 const square = d3.selectAll("rect");
 square.style("fill", "orange"); 
+
+
+
+
+    var treeData = [
+    {
+        "name": "Top Level",
+        "parent": "null",
+        "icon":"../plane.png",
+        "children": [
+            {
+                "name": "Level 2: A",
+                "parent": "Top Level",
+                "children": [
+                    {
+                        "name": "Son of A",
+                        "parent": "Level 2: A"
+                    },
+                    {
+                        "name": "Daughter of A",
+                        "parent": "Level 2: A"
+                    }
+                ]
+            },
+            {
+                "name": "Level 2: B",
+                "parent": "Top Level"
+            }
+        ]
+    }
+];
+    var margin = { top: 20, right: 120, bottom: 20, left: 120 },
+        width = 960 - margin.right - margin.left,
+        height = 500 - margin.top - margin.bottom;
+
+    var i = 0;
+
+    var tree = d3.layout.tree()
+        .size([height, width]);
+
+    var diagonal = d3.svg.diagonal()
+        .projection(function (d) { return [d.y, d.x]; });
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+    root = treeData[0];
+
+    update(root);
+
+    function update(source) {
+
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+            links = tree.links(nodes);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function (d) { d.y = d.depth * 180; });
+
+        // Declare the nodes…
+        var node = svg.selectAll("g.node")
+            .data(nodes, function (d) { return d.id || (d.id = ++i); });
+
+        // Enter the nodes.
+        var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                return "translate(" + d.y + "," + d.x + ")";
+            });
+
+        nodeEnter.append("image")
+            .attr("xlink:href", function (d) { return d.icon; })
+            .attr("x", "-12px")
+            .attr("y", "-12px")
+            .attr("width", "24px")
+            .attr("height", "24px");
+
+        nodeEnter.append("text")
+            .attr("x", function (d) {
+                return d.children || d._children ?
+                    (15) * -1 : + 15
+            })
+            .attr("dy", ".35em")
+            .attr("text-anchor", function (d) {
+                return d.children || d._children ? "end" : "start";
+            })
+            .text(function (d) { return d.name; })
+            .style("fill-opacity", 1);
+
+        // Declare the links…
+        var link = svg.selectAll("path.link")
+            .data(links, function (d) { return d.target.id; });
+
+        // Enter the links.
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .style("stroke", function (d) { return d.target.level; })
+            .attr("d", diagonal);
+
+    }
